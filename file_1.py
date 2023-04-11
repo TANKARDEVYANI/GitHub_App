e3fcbba2a135376c1241263f80ac9549e62c27d1
TANKARDEVYANI <130478912+TANKARDEVYANI@users.noreply.github.com>
2023-04-11 21:17:10 +0530

    Update file_1.py
    added emojis

--- file_1.py
+++ file_1.py
@@ -1,7 +1,10 @@
 import streamlit
 
-streamlit.title('My parents new healty Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale,Spanich and Rocket Smoothie')
-streamlit.text('Hard Boiled Free-Ranged Egg')
+streamlit.title('My Mom's new healty Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗Kale,Spanich and Rocket Smoothie')
+streamlit.text('🐔Hard Boiled Free-Ranged Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+                streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
